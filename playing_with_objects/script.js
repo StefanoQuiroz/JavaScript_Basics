bbd3894ce602ed62a69c6bf0761855390ac06a1c
Piero <piero.quiroz93@gmail.com>
2021-03-30 23:05:29 -0500

    Playing with Objects

--- playing_with_objects/script.js
+++ playing_with_objects/script.js
@@ -15,7 +15,12 @@ var users = [
     }
 ];
 
-//¿Cómo harías print/log de la edad de John?
+// ¿Cómo harías print/log de la edad de John?
 console.log(`Edad de John ${users[1].age}`);
-//¿Cómo harías print/log del nombre del primer objeto?
+// ¿Cómo harías print/log del nombre del primer objeto?
 console.log(`Nombre del primer Objeto: ${users[0]['name']}`);
+// ¿Cómo harías print/log del nombre y la edad de cada usuario utilizando un for loop? Tu output debería verse algo como esto
+for(var i in users){
+    console.log(`${users[i].name} - ${users[i].age}`);
+}
+// ¿Cómo harías para imprimir el nombre de los mayores de edad?
