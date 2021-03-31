01bc3416900bb57de242f37f4168f8c2ab4dbf34
Piero <piero.quiroz93@gmail.com>
2021-03-30 20:39:10 -0500

    Objects 2

--- objects/script2.js
+++ objects/script2.js
@@ -1,1 +1,12 @@
 // medium entendiendo javascript
+// Objetos literales o declarativos sin instanciar o hacer un constructor
+const stefano = {
+    nombre : 'Stefano',
+    edad: 27,
+    sexo: 'masculino',
+    pasatiempos : ['correr', 'sauna'],
+    hablar : function(){return `Hola soy ${this.nombre}, y tengo ${this.edad} años`;}
+}
+
+console.log(stefano.hablar());
+// Objetos construidos
