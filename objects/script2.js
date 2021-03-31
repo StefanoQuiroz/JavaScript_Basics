8cd740cd53092d3cc318d7b287e46b519df7f0c8
Piero <piero.quiroz93@gmail.com>
2021-03-30 20:52:22 -0500

    Objects 2

--- objects/script2.js
+++ objects/script2.js
@@ -1,14 +1,24 @@
 // medium entendiendo javascript
 // Objetos literales o declarativos sin instanciar o hacer un constructor
 // () => {this.key no usar} function() {usar this.key}
-const stefano = {
-    nombre : 'Stefano',
-    edad: 27,
-    sexo: 'masculino',
-    pasatiempos : ['correr', 'sauna'],
-    hablar : function(){return `Hola soy ${this.nombre}, y tengo ${this.edad} años`;}
+// const stefano = {
+//     nombre : 'Stefano',
+//     edad: 27,
+//     sexo: 'masculino',
+//     pasatiempos : ['correr', 'sauna'],
+//     hablar : function(){return `Hola soy ${this.nombre}, y tengo ${this.edad} años`;}
+// }
+
+//console.log(stefano.hablar());
+// Objetos construidos
+function Persona(nombre, edad, sexo, pasatiempos){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.pasatiempos = pasatiempos;
+    this.hablar = function(){return `Hola soy ${this.nombre} y tengo ${this.edad} años`;};
 }
 
-console.log(stefano.hablar());
-// Objetos construidos
-function Persona(nombre)
+const stefan = new Persona('Stefano', 27, 'masculino', ['correr', 'nadar']);
+
+console.log(stefan.hablar());
