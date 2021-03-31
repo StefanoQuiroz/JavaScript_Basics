b265c7c6a9af936cb184cf3f9f0fdc71cf34b126
Piero <piero.quiroz93@gmail.com>
2021-03-30 23:00:00 -0500

    Playing with Objects

---
+++ playing_with_objects/script.js
@@ -0,0 +1,21 @@
+// 1 Practica iterar por medio de un array de objetos/diccionarios.
+
+var users = [
+    {
+        name: 'Michael',
+        age: 37
+    },
+    {
+        name: 'John',
+        age: 30
+    },
+    {
+        name: 'David',
+        age: 27
+    }
+];
+
+//¿Cómo harías print/log de la edad de John?
+console.log(`Edad de John ${users[1].age}`);
+//¿Cómo harías print/log del nombre del primer objeto?
+console.log(`Nombre del primer Objeto: ${users[0]['name']}`);
